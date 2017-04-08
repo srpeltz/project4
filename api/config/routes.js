8aa6119b52d24391751e95e48d50ea329bc815d2
steven peltzer <srpeltzer@yahoo.com>
2017-04-08 14:32:21 -0700

    add routes and .env file and token_auth.js

--- api/config/routes.js
+++ api/config/routes.js
@@ -2,3 +2,17 @@ var express         = require('express')
 var router          = express.Router()
 var bodyParser      = require('body-parser')
 var methodOverride  = require('method-override')
+
+var usersController = require('../controllers/users')
+var token           = require('./token_auth')
+
+//users routes
+router.route('/api/users')
+  .post(usersController.create)
+router.route('/api/token')
+  .post(token.create)
+router.route('/api/me')
+  .get(token.authenticate, usersController.me)
+
+
+module.exports = router
