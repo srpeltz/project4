2b841607535534227e77476e1addba77d60f2f2e
steven peltzer <srpeltzer@yahoo.com>
2017-04-10 15:15:35 -0700

    sign in and sign up functionality added

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -6,6 +6,10 @@ function RecipeRouter($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise("/")
 
   $stateProvider
+  .state('index', {
+    url:'/',
+    templateUrl: 'index.html'
+  })
   .state('signin', {
     url: '/signin',
     templateUrl: 'signin.html',
