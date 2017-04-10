ce5b9d34d80a33ec0ef8b44af670eb95cb672b55
steven peltzer <srpeltzer@yahoo.com>
2017-04-10 14:43:50 -0700

    render sign-in & sign-up views

--- api/models/User.js
+++ api/models/User.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose')
 
 var userSchema = new mongoose.Schema({
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   name:  { type: String, required: true }
 })
 
