bdfdb0e0c653aab119a60eb0e3bcd6782fb37ee8
steven peltzer <srpeltzer@yahoo.com>
2017-04-12 10:07:20 -0700

    removed NavbarController not a function error

--- frontend/js/api/recipeFactory.js
+++ frontend/js/api/recipeFactory.js
@@ -7,10 +7,15 @@ function RecipeFactory($http) {
   var apiUrl = 'http://food2fork.com/api/search?key=542b55facc7ab92140bab83167fdd2af'
 
   return {
-    index: index
+    index: index,
+    show: show
   }
 
   function index() {
+    return $http.get(apiUrl)
+  }
+
+  function show(recipe) {
     return $http.get(apiUrl)
   }
 }
